b4e2d3a195b18a31523133e054482ff7c8cd7ff4
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2023-01-08 12:39:28 +0200

    Player Movement added

--- Harvest/Assets/Scripts/PlayerScripts/PlayerInput.cs
+++ Harvest/Assets/Scripts/PlayerScripts/PlayerInput.cs
@@ -4,15 +4,102 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region PROPERTIES
+
+    #region Delegates
+    #endregion
+
+    [Header("Player GameObject")]
+    GameObject player;
+    PlayerMovement playerMovement;
+
+
+    // Moving
+    Vector3 moveInput = Vector3.zero;
+
+    #endregion
+
+
+    #region SET-UP
+
+    private void Start()
     {
-        
+        // Get Player
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        player = playerMovement.gameObject;
+        if (!player) { Debug.Log("No Player Found!"); }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ListenForInputs();
+
+        // Execute Movement Inputs
+        ExecuteInputs();
     }
+    #endregion
+
+    #region INPUTS
+    private void ListenForInputs()
+    {
+        GetMoveInputs();
+    }
+
+    private void GetMoveInputs()
+    {
+
+        // Reset Movement Vector
+        moveInput = Vector3.zero;
+
+        // Get the WASD inputs
+        float ws_input = 0;
+        float ad_input = 0;
+
+        #region Keypress detection
+        // Check W / Up
+        if (Input.GetKey(KeyCode.W))
+        { ws_input += 1; }
+
+        // Check S / Down
+        if (Input.GetKey(KeyCode.S))
+        { ws_input += -1; }
+
+        // Check A / Left
+        if (Input.GetKey(KeyCode.A))
+        { ad_input += -1; }
+
+        // Check D / Right
+        if (Input.GetKey(KeyCode.D))
+        { ad_input += 1; }
+        #endregion
+
+        // Add the input values to the Movement Vector
+        moveInput = new Vector3(ad_input, 0f, ws_input);
+
+        // Normalize the Vector
+        moveInput = moveInput.normalized;
+
+    }
+    #endregion
+
+    #region EXECUTION
+    private void ExecuteInputs()
+    {
+        SendMoveInputs(moveInput);
+    }
+
+    private void SendMoveInputs(Vector3 moveVector)
+    {
+        if (moveVector == Vector3.zero) /* There was no movement */ { return; }
+
+        if (playerMovement == null) /* There is no Player */ { return; }
+
+        // Send Inputs
+        playerMovement.MovePlayer(moveInput);
+
+        //Debug.Log("MoveInput: " + moveInput);
+
+    }
+
+    #endregion
 }
