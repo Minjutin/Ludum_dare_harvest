3910a403b15505220f541126d85608ef1a1fd215
Milja <91054318+Minjutin@users.noreply.github.com>
2023-01-09 14:57:10 +0200

    Menu stuff

---
+++ Harvest/Assets/MenuStuff/Choser.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choser : MonoBehaviour
+{
+
+    float movement = 5;
+
+    GameObject p1indicator;
+    GameObject p2indictor;
+
+    Enums.God god1;
+    Enums.God god2;
+    Enums.God god3;
+    Enums.God god4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if()
+
+    }
+}
