5b6dab70cd58d313bc3764d635fc5a0e12057211
Milja <91054318+Minjutin@users.noreply.github.com>
2023-01-13 13:57:53 +0200

    Tutorial

---
+++ Harvest/Assets/MenuStuff/TutorialScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour
+{
+    int currentSlide = 0;
+
+    [SerializeField] List<Sprite> slides;
+    [SerializeField] GameObject chooser;
+
+    Image thisSprite;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        thisSprite = transform.Find("Background").gameObject.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        //Continue if any interaction button is pressed
+        if (Input.GetKeyDown(Statics.p2interaction) || Input.GetKeyDown(Statics.p2inventory) || Input.GetKeyDown(Statics.p1interaction) || Input.GetKeyDown(Statics.p1inventory) || Input.GetKeyDown(KeyCode.Space))
+        {
+
+            //If there are any tutorial slides left
+            if(currentSlide < slides.Count - 1)
+            {
+                currentSlide++;
+                thisSprite.sprite = slides[currentSlide];
+            }
+
+            //Otherwise open the character choose
+
+            else
+            {
+                chooser.SetActive(true);
+                this.gameObject.SetActive(false);
+            }
+
+
+        }
+    }
+}
