b0aa866e485d782cc904ce62df01226a719dcbf7
Milja <91054318+Minjutin@users.noreply.github.com>
2023-01-14 20:19:49 +0200

    Disable players too

--- Harvest/Assets/Scripts/GameManager/GameManager.cs
+++ Harvest/Assets/Scripts/GameManager/GameManager.cs
@@ -7,31 +7,35 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        DisableGods(); 
+        DisablePlayers();
     }
 
     // Disable unused gods in the start
-    public void DisableGods()
+    public void DisablePlayers()
     {
 
         if(Statics.p1God!=Enums.God.God1 && Statics.p2God != Enums.God.God1)
         {
             GameObject.Find("God1").SetActive(false);
+            GameObject.Find("P1").SetActive(false);
         }
 
         if (Statics.p1God != Enums.God.God2 && Statics.p2God != Enums.God.God2)
         {
             GameObject.Find("God2").SetActive(false);
+            GameObject.Find("P2").SetActive(false);
         }
 
         if (Statics.p1God != Enums.God.God3 && Statics.p2God != Enums.God.God3)
         {
             GameObject.Find("God3").SetActive(false);
+            GameObject.Find("P3").SetActive(false);
         }
 
         if (Statics.p1God != Enums.God.God4 && Statics.p2God != Enums.God.God4)
         {
             GameObject.Find("God4").SetActive(false);
+            GameObject.Find("P4").SetActive(false);
         }
     }
 }
